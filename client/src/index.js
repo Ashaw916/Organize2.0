b14809a11b7096229afb8c852e6a8ce24d6f6433
Aaron Young <aaron@auctionmethod.com>
2020-08-24 21:00:53 -0700

    fixed a mistake I made in index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
