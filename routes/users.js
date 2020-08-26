28422551264f4551b069820b108b616500881ee3
Aaron Young <aaron@auctionmethod.com>
2020-08-25 20:38:50 -0700

    passport progress

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,10 @@ const apiRoutes = require("./api");
 
 // API Routes
 router.use("/api", apiRoutes);
-router.get("/", (req, res) => res.send("welcome"));
+//login route
+router.get("/login", (req, res) => res.send("login"));
+//register route
+router.get("/register", (req, res) => res.send("register"));
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
--- server.js
+++ server.js
@@ -2,21 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/organize");
-
+// mongodb+srv://admin:<password>@cluster0.ozd49.mongodb.net/<dbname>?retryWrites=true&w=majority
 // Start the API server
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
@@ -29,4 +36,4 @@ console.log();
 //use passport
 //use bcrypt for encryption
 //send invitations via sms. use bcrypt to encrypt the phone number
-//create internal api key and store in .env so that outside calls wont be accepted
+//create internal api key and store in .env so that outside calls wont be
