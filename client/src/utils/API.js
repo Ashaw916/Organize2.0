7f0aeaf3ddc5bc08a7770e029c5adf2f368de9e4
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-08-25 21:04:00 -0700

    adjusted utils/API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   // Gets all events
-  getevents: function () {
+  getEvents: function () {
     return axios.get("/api/events");
   },
   // Gets the event with the given id
@@ -13,8 +13,33 @@ export default {
   deleteEvent: function (id) {
     return axios.delete("/api/events/" + id);
   },
-  // Saves a event to the database
+  // Saves an event to the database
   saveEvent: function (eventData) {
     return axios.post("/api/events", eventData);
   },
+  // Updates an event
+  updateEvent: function (id, eventData) {
+    return axios.put("/api/events/" + id, eventData);
+  },
+
+  // Gets all articles
+  getArticles: function () {
+    return axios.get("/api/articles");
+  },
+  // Gets the article with the given id
+  getArticle: function (id) {
+    return axios.get("/api/articles/" + id);
+  },
+  // Deletes the article with the given id
+  deleteArticle: function (id) {
+    return axios.delete("/api/articles/" + id);
+  },
+  // Saves an article to the database
+  saveArticle: function (articleData) {
+    return axios.post("/api/articles", articleData);
+  },
+  // Updates an article
+  updateArticle: function (id, articleData) {
+    return axios.put("/api/articles/" + id, articleData);
+  }
 };
