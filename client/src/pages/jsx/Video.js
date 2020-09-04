3757c03be6f7edf5ffc667423dc54910b6ecc47b
Vanessa Bautista <vbautista916@icloud.com>
2020-09-04 10:24:39 -0700

    video archive now connected to backend mongodb

--- client/src/pages/jsx/Video.js
+++ client/src/pages/jsx/Video.js
@@ -1,52 +1,59 @@
-import React, { Component } from "react";
-import { getVideos } from "../../resources/videos";
+import React, { useState, useEffect } from "react";
+// import { getVideos } from "../../resources/videos";
+import API from "../../utils/API";
 import VideoLiveStream from "../../components/VideoLiveStream/VideoLiveStream";
 
 // import { Link, Route } from "react-router-dom";
 // import VideoArchive from "../../components/VideoArchive/VideoArchive.js";
 
-class Video extends Component {
-  state = {
-    videos: getVideos(),
-  };
+function Video() {
+  const [videos, setVideos] = useState([]);
 
-  render() {
-    return (
-      <>
-        <div className="container">
-          <div className="row">
-            <h1>Video Page</h1>
-          </div>
-          <div className="row">
-            <VideoLiveStream />
-          </div>
+  useEffect(() => {
+    loadVideos();
+  }, []);
+  // Loads all books and sets them to books
+  function loadVideos() {
+    API.getVideos()
+      .then((res) => setVideos(res.data))
+      .catch((err) => console.log(err));
+  }
 
-          <div className="row">
-            <h2>Video Links Archive</h2>
-          </div>
-          <div className="row">
-            <div className="col">
-              <div className="list-group">
-                {this.state.videos.map((video) => (
-                  <div className="list-group-item list-group-item-action">
-                    <iframe
-                      style={{ height: "300px", width: "auto" }}
-                      src={video.src}
-                      allowfullscreen
-                    ></iframe>
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <h1>Video Page</h1>
+        </div>
+        <div className="row">
+          <VideoLiveStream />
+        </div>
 
-                    <h5> Title: {video.title} </h5>
-                    <p>Description: {video.description}</p>
-                    <a href={video.src}>Source: {video.src}</a>
-                  </div>
-                ))}
-              </div>
+        <div className="row">
+          <h2>Video Links Archive</h2>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="list-group">
+              {videos.map((video) => (
+                <div className="list-group-item list-group-item-action">
+                  <iframe
+                    style={{ height: "300px", width: "auto" }}
+                    src={video.src}
+                    // allowfullscreen
+                  ></iframe>
+
+                  <h5> Title: {video.title} </h5>
+                  <p>Description: {video.description}</p>
+                  <a href={video.src}>Source: {video.src}</a>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default Video;
