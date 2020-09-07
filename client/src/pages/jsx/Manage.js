17b506691ae52685382a4e5829d0880c1037ace0
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-09-06 19:24:46 -0700

    debug calendar info block and manage page

--- client/src/pages/jsx/Manage.js
+++ client/src/pages/jsx/Manage.js
@@ -13,7 +13,7 @@ function Manage() {
   const [getEvents, setGetEvents] = useState([]);
   const [getArticles, setGetArticles] = useState([]);
   const [getVideos, setGetVideos] = useState([]);
-//functions to load arrays of objects for the page
+  //functions to load arrays of objects for the page
   function loadEvents() {
     API.getEvents()
       .then((res) => {
@@ -40,7 +40,7 @@ function Manage() {
       })
       .catch((err) => console.log(err));
   };
-// three indiviual use effects to load events, articles and videos
+  // three indiviual use effects to load events, articles and videos
   useEffect(() => {
     loadEvents();
   }, []);
@@ -53,7 +53,7 @@ function Manage() {
     loadVideos();
   }, []);
 
-//all three functions delete an event, article or video using the id for the document
+  //all three functions delete an event, article or video using the id for the document
   function deleteEvent(id) {
     API.deleteEvent(id)
       .then(res => loadEvents())
@@ -73,104 +73,104 @@ function Manage() {
   }
 
 
-//////////////////////// FOR EVENT FORM ///////////////////////// 
-//state for values that can go straight into state
-// const [formObject, setFormObject] = useState({});
-// //all other states for values that must be modified before pushing to backend
-// const [start, ]
+  //////////////////////// FOR EVENT FORM ///////////////////////// 
+  //state for values that can go straight into state
+  // const [formObject, setFormObject] = useState({});
+  // //all other states for values that must be modified before pushing to backend
+  // const [start, ]
 
- //cocatenate dates and times together with a T together
- //will need to convert 12hr times to 24 before concatenating
+  //cocatenate dates and times together with a T together
+  //will need to convert 12hr times to 24 before concatenating
 
 
 
-    return (
-      <>
-        <div className="container">
-          <h1>Administration Management Page</h1>
-          <div className="row">
-            <div className="col-5 m-1">
-              <AddEvent 
-              
-              />
-            </div>
-            <div className="col-6 m-1">
-              <div className="card">
-                <div className="card-header">Your posted events</div>
-                <div className="card-body">
-                  <ul className="list-group list-group-flush">
-                    {getEvents.map((event) => (
-                      <li className="list-group-item" key={event._id}>
-                        {event.title}: {event.start_date}{" "}
-                        <button
-                          type="button"
-                          className="btn btn-danger btn-sm"
-                          onClick={() => deleteEvent(event._id)}
-                        >
-                          Delete
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
+  return (
+    <>
+      <div className="container">
+        <h1>Administration Management Page</h1>
+        <div className="row">
+          <div className="col-5 m-1">
+            <AddEvent
+
+            />
           </div>
-          <div className="row">
-            <div className="col-5 m-1">
-              <AddResource />
-            </div>
-            <div className="col-6 m-1">
-              <div className="card">
-                <div className="card-header">Your posted articles</div>
-                <div className="card-body">
-                  <ul className="list-group list-group-flush">
-                    {getArticles.map((article) => (
-                      <li className="list-group-item" key={article._id}>
-                        {article.title}
-                        <button 
-                        type="button" 
+          <div className="col-6 m-1">
+            <div className="card">
+              <div className="card-header">Your posted events</div>
+              <div className="card-body">
+                <ul className="list-group list-group-flush">
+                  {getEvents.map((event) => (
+                    <li className="list-group-item" key={event._id}>
+                      {event.title}: {event.start_date}{" "}
+                      <button
+                        type="button"
                         className="btn btn-danger btn-sm"
-                        onClick={() => deleteArticle(article._id)}
-                        >
-                          Delete
+                        onClick={() => deleteEvent(event._id)}
+                      >
+                        Delete
                         </button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-5 m-1">
-              <AddVideo />
-            </div>
-            <div className="col-6 m-1">
-              <div className="card">
-                <div className="card-header">Your posted videos</div>
-                <div className="card-body">
-                  <ul className="list-group list-group-flush">
-                    {getVideos.map((video) => (
-                      <li className="list-group-item" key={video._id}>
-                        {video.title}
-                        <button 
-                        type="button" 
-                        className="btn btn-danger btn-sm"
-                        onClick={() => deleteVideo(video._id)}
-                        >
-                          Delete
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+        <div className="row">
+          <div className="col-5 m-1">
+            <AddResource />
+          </div>
+          <div className="col-6 m-1">
+            <div className="card">
+              <div className="card-header">Your posted articles</div>
+              <div className="card-body">
+                <ul className="list-group list-group-flush">
+                  {getArticles.map((article) => (
+                    <li className="list-group-item" key={article._id}>
+                      {article.title}
+                      <button
+                        type="button"
+                        className="btn btn-danger btn-sm"
+                        onClick={() => deleteArticle(article._id)}
+                      >
+                        Delete
+                        </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-5 m-1">
+            <AddVideo />
+          </div>
+          <div className="col-6 m-1">
+            <div className="card">
+              <div className="card-header">Your posted videos</div>
+              <div className="card-body">
+                <ul className="list-group list-group-flush">
+                  {getVideos.map((video) => (
+                    <li className="list-group-item" key={video._id}>
+                      {video.title}
+                      <button
+                        type="button"
+                        className="btn btn-danger btn-sm"
+                        onClick={() => deleteVideo(video._id)}
+                      >
+                        Delete
+                        </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Manage;
