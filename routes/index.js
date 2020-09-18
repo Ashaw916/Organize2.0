d3e66084ad1bfa89a75594094424ceb93b0f9d68
Sandra <sandra.alpha2010@gmail.com>
2020-09-18 13:54:07 -0700

    adding a missing line of code in index.js of routes folder to send React app if no api routes are hit

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 router.get("/", (req, res) => res.send("welcome"));
 
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
 module.exports = router;
