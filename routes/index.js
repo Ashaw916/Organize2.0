96f4c3e2e12735ec125b4268ac6a9340388b18e2
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-09-18 18:07:31 -0700

    fixed duplicate path require issue in index routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const path = require("path");
 
 // API Routes
 router.use("/api", apiRoutes);
